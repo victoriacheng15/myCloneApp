9cbcd6285a032a1199cc30ebe20fb7e1aae8b3e2
Victoria Cheng <victoria.cheng15@gmail.com>
2025-01-20 11:19:52 -0700

    edit style for the looking and names

--- components/userInfo.tsx
+++ components/userInfo.tsx
@@ -3,22 +3,24 @@ import { StyleSheet, Text, View, Image } from "react-native";
 export default function UserInfo() {
 	return (
 		<View style={styles.container}>
-			<Image
-				source={{ uri: "https://picsum.photos/200" }}
-				style={styles.image}
-			/>
-			<View style={styles.infoContainer}>
-				<View style={styles.info}>
-					<Text style={styles.infoFont}>53</Text>
-					<Text style={styles.infoFont2}>Posts</Text>
-				</View>
-				<View style={styles.info}>
-					<Text style={styles.infoFont}>12</Text>
-					<Text style={styles.infoFont2}>Members</Text>
-				</View>
-				<View style={styles.info}>
-					<Text style={styles.infoFont}>1</Text>
-					<Text style={styles.infoFont2}>Admins</Text>
+			<View style={styles.profileHeader}>
+				<Image
+					source={{ uri: "https://picsum.photos/200" }}
+					style={styles.image}
+				/>
+				<View style={styles.profileStats}>
+					<View style={styles.info}>
+						<Text style={styles.infoValue}>53</Text>
+						<Text style={styles.infoLabel}>Posts</Text>
+					</View>
+					<View style={styles.info}>
+						<Text style={styles.infoValue}>12</Text>
+						<Text style={styles.infoLabel}>Members</Text>
+					</View>
+					<View style={styles.info}>
+						<Text style={styles.infoValue}>1</Text>
+						<Text style={styles.infoLabel}>Admins</Text>
+					</View>
 				</View>
 			</View>
 		</View>
@@ -28,24 +30,32 @@ export default function UserInfo() {
 const styles = StyleSheet.create({
 	container: {
 		display: "flex",
-		flexDirection: "row",
-		justifyContent: "space-between",
+		flexDirection: "column",
 		alignItems: "center",
 		width: "100%",
-		padding: 16,
+		paddingVertical: 16,
 		gap: 30,
+	},
+	profileHeader: {
+		display: "flex",
+		flexDirection: "row",
+		width: "100%",
+		justifyContent: "space-between",
+		alignItems: "center",
+		paddingHorizontal: 16,
+		gap: 16,
+	},
+	profileStats: {
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		gap: 16,
 	},
 	image: {
 		width: 100,
 		height: 100,
 		borderRadius: 50,
-	},
-	infoContainer: {
-		display: "flex",
-		flexDirection: "row",
-		justifyContent: "center",
-		alignItems: "center",
-		gap: 20,
 	},
 	info: {
 		display: "flex",
@@ -54,11 +64,11 @@ const styles = StyleSheet.create({
 		alignItems: "center",
 		gap: 4,
 	},
-	infoFont: {
+	infoValue: {
 		fontSize: 20,
 		fontWeight: "bold",
 	},
-	infoFont2: {
+	infoLabel: {
 		fontSize: 14,
 		color: "gray",
 	},
